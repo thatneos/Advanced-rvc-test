9c88c103c5d672c72fc1238d7e249431aa8b04fe
Jarod Mica <jarodmica@gmail.com>
2023-08-10 01:52:15 -0700

    Update some files to make certain modules installable

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the requirements.txt file
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='rvc',
     version='0.1',
     description='rvc package',
     author='RVC people',
     packages=find_packages(),
+    install_requires=requirements,  # Use the list of requirements here
 )
