acd2937e003c97fa5dc118a04337593fc469dfb7
Jarod Mica <jarodmica@gmail.com>
2023-08-10 00:29:07 -0700

    Removed poetry setup, incorporated a basic setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rvc',
+    version='0.1',
+    description='rvc package',
+    author='RVC people',
+    packages=find_packages(),
+)
