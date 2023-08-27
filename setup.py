d57ce226c3a904658596004abdeb53e458497f74
Jarod Mica <jarodmica@gmail.com>
2023-08-27 15:58:54 -0700

    small fix

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
-import pkg_resources
 
 # Read the requirements.txt file
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
-
-path_to_json = pkg_resources.resource_filename('rvc', 'lib/uvr5_pack/lib_v5/modelparams/4band_v2.json')
 
 setup(
     name='rvc',
